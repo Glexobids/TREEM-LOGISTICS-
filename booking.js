a1ec39508522e1d605f70d2b57243151ac0aed79
Glexobids <glexobids@gmail.com>
2024-09-08 18:12:21 +0530

    Update booking.js

--- booking.js
+++ booking.js
@@ -1,1 +1,8 @@
+const bookings = [
+  { id: 1, truckType: 'MCV', rentalDuration: 3, pickupDate: '2024-09-10', returnDate: '2024-09-13' },
+  { id: 2, truckType: 'HCV', rentalDuration: 5, pickupDate: '2024-09-15', returnDate: '2024-09-20' },
+  // Add more bookings here
+];
+
+export default bookings;
 
